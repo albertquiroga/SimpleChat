41c6ad9810a553740e9ce37717eff1da75d49eee
aquirogb <pyro_quiro@hotmail.com>
2016-11-11 12:29:43 +0100

    Added server-side proper printing

--- src/main/java/com/pyrospade/simplechat/message/MessagePool.java
+++ src/main/java/com/pyrospade/simplechat/message/MessagePool.java
@@ -16,7 +16,7 @@ public class MessagePool extends Observable {
     }
 
     public void addMessage(Message m) {
-        System.out.println("\t" + m.getContent());
+        System.out.println("\t" + m.getChatLine());
         pool.put(m.getId(),m);
         setChanged();
         notifyObservers(m);
